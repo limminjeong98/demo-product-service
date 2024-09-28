e4281a4bee55ff1915511f7f929abeb4d3b08b68
limminjeong98 <limminjeong98@naver.com>
2024-09-28 17:58:48 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/demoproductservice/DemoProductServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProductServiceApplication.class, args);
+    }
+
+}
