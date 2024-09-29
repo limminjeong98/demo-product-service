56b954dfddd02438caa2dab0faeeadd155e0f20a
limminjeong98 <limminjeong98@naver.com>
2024-09-29 11:50:01 +0900

    test: 리포지토리 레이어 테스트 환경 설정

---
+++ src/test/resources/data.sql
@@ -0,0 +1,36 @@
+INSERT INTO brands(id, brand_name) values(1, 'A');
+INSERT INTO brands(id, brand_name) values(2, 'B');
+INSERT INTO brands(id, brand_name) values(3, 'C');
+INSERT INTO brands(id, brand_name) values(4, 'D');
+INSERT INTO brands(id, brand_name) values(5, 'E');
+INSERT INTO brands(id, brand_name) values(6, 'F');
+INSERT INTO brands(id, brand_name) values(7, 'G');
+INSERT INTO brands(id, brand_name) values(8, 'H');
+INSERT INTO brands(id, brand_name) values(9, 'I');
+
+INSERT INTO categories(id, category_type) values (1, 'TOP');
+INSERT INTO categories(id, category_type) values (2, 'OUTER');
+INSERT INTO categories(id, category_type) values (3, 'BOTTOM');
+INSERT INTO categories(id, category_type) values (4, 'SNEAKERS');
+INSERT INTO categories(id, category_type) values (5, 'BAG');
+INSERT INTO categories(id, category_type) values (6,'HAT');
+INSERT INTO categories(id, category_type) values (7, 'SOCKS');
+INSERT INTO categories(id, category_type) values (8, 'ACCESSORY');
+
+INSERT INTO products(id, category_id, brand_id, price) values (1, 1, 1, 11200);
+INSERT INTO products(id, category_id, brand_id, price) values (2, 2, 1, 5500);
+INSERT INTO products(id, category_id, brand_id, price) values (3, 3, 1, 4200);
+INSERT INTO products(id, category_id, brand_id, price) values (4, 4, 1, 9000);
+INSERT INTO products(id, category_id, brand_id, price) values (5, 5, 1, 2000);
+INSERT INTO products(id, category_id, brand_id, price) values (6, 6, 1, 1700);
+INSERT INTO products(id, category_id, brand_id, price) values (7, 7, 1, 1800);
+INSERT INTO products(id, category_id, brand_id, price) values (8, 8, 1, 2300);
+
+INSERT INTO products(id, category_id, brand_id, price) values (9, 1, 2, 10500);
+INSERT INTO products(id, category_id, brand_id, price) values (10, 2, 2, 5900);
+INSERT INTO products(id, category_id, brand_id, price) values (11, 3, 2, 3800);
+INSERT INTO products(id, category_id, brand_id, price) values (12, 4, 2, 9100);
+INSERT INTO products(id, category_id, brand_id, price) values (13, 5, 2, 2100);
+INSERT INTO products(id, category_id, brand_id, price) values (14, 6, 2, 2000);
+INSERT INTO products(id, category_id, brand_id, price) values (15, 7, 2, 2000);
+INSERT INTO products(id, category_id, brand_id, price) values (16, 8, 2, 2200);
