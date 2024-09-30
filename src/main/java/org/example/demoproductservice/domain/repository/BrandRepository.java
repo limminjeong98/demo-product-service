08297d768a2b0d39b3cddb9189c2de55009f5604
limminjeong98 <limminjeong98@naver.com>
2024-09-30 14:22:36 +0900

    feat: 브랜드 조회, 등록, 수정, 삭제 서비스 및 리포지토리 구현

--- src/main/java/org/example/demoproductservice/domain/repository/BrandRepository.java
+++ src/main/java/org/example/demoproductservice/domain/repository/BrandRepository.java
@@ -4,6 +4,10 @@ import org.example.demoproductservice.domain.repository.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Brand> findById(Long id);
+    void deleteById(Long id);
 }
