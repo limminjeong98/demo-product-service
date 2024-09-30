a8755ee0775968a368c375d3c2b7c08b9e02fe6f
limminjeong98 <limminjeong98@naver.com>
2024-09-30 09:57:11 +0900

    test: 단일 브랜드로 전체 카테고리 상품을 구매할 경우, 상품 가격 총액이 가장 낮은 브랜드의 코디 정보 조회 기능 테스트

--- src/test/java/org/example/demoproductservice/application/ProductFacadeTest.java
+++ src/test/java/org/example/demoproductservice/application/ProductFacadeTest.java
@@ -1,10 +1,14 @@
 package org.example.demoproductservice.application;
 
+import org.example.demoproductservice.common.exception.BrandNotFoundException;
 import org.example.demoproductservice.common.exception.ProductNotFoundException;
+import org.example.demoproductservice.domain.repository.BrandRepositoryStub;
 import org.example.demoproductservice.domain.repository.CategoryRepositoryStub;
 import org.example.demoproductservice.domain.repository.ProductRepositoryStub;
+import org.example.demoproductservice.domain.repository.entity.Brand;
 import org.example.demoproductservice.domain.repository.entity.Category;
 import org.example.demoproductservice.domain.repository.entity.Product;
+import org.example.demoproductservice.domain.service.BrandService;
 import org.example.demoproductservice.domain.service.CategoryService;
 import org.example.demoproductservice.domain.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.example.demoproductservice.domain.repository.entity.Category.CategoryType.*;
@@ -40,6 +45,12 @@ class ProductFacadeTest {
 
     @InjectMocks
     ProductFacade sut;
+
+    @Spy
+    BrandRepositoryStub brandRepository;
+
+    @Mock
+    BrandService brandService;
 
     @DisplayName("각 카테고리별 가격이 가장 낮은 상품으로 구성된 코디 정보를 조회한다")
     @Nested
@@ -127,4 +138,112 @@ class ProductFacadeTest {
     }
 
 
+    @DisplayName("단일 브랜드로 전체 카테고리 상품을 구매할 경우, 상품 가격 총액이 가장 낮은 코디 정보를 조회한다")
+    @Nested
+    class GetLowestPriceBrandProductSet {
+        @DisplayName("각 브랜드의 카테고리별 상품 가격 총액이 가장 낮은 코디 상품을 반환하는지 검증한다")
+        @Test
+        void test1() {
+            // given
+            // 테스트 데이터 세팅
+            brandRepository.prepareTestData();
+            categoryRepository.prepareTestData();
+            productRepository.prepareTestData();
+
+            List<Brand> brands = brandRepository.findAll();
+            given(brandService.findAll()).willReturn(brands);
+
+            List<Category> categories = categoryRepository.findAll();
+            given(categoryService.findAll()).willReturn(categories);
+
+            for (Brand brand : brands) {
+                for (Category category : categories) {
+                    Product product = productRepository.findTopByBrandAndCategoryOrderByPriceAsc(brand, category);
+                    given(productService.findLowestPriceProductByBrandAndCategory(brand, category)).willReturn(product);
+                }
+            }
+
+            // when
+            BrandCoordSet result = sut.getLowestPriceBrandProductSet();
+
+            // then
+            assertNotNull(result);
+            assertEquals("D", result.brandName());
+            assertEquals(36100L, result.totalPrice());
+            List<BrandCoordItem> items = result.items();
+            for (BrandCoordItem item : items) {
+                if (item.categoryType().equals(TOP.name())) {
+                    assertEquals(10100L, item.price());
+                } else if (item.categoryType().equals(OUTER.name())) {
+                    assertEquals(5100L, item.price());
+                } else if (item.categoryType().equals(BOTTOM.name())) {
+                    assertEquals(3000L, item.price());
+                } else if (item.categoryType().equals(SNEAKERS.name())) {
+                    assertEquals(9500L, item.price());
+                } else if (item.categoryType().equals(BAG.name())) {
+                    assertEquals(2500L, item.price());
+                } else if (item.categoryType().equals(HAT.name())) {
+                    assertEquals(1500L, item.price());
+                } else if (item.categoryType().equals(SOCKS.name())) {
+                    assertEquals(2400L, item.price());
+                } else if (item.categoryType().equals(ACCESSORY.name())) {
+                    assertEquals(2000L, item.price());
+                }
+            }
+        }
+
+
+        @DisplayName("브랜드의 일부 카테고리에 상품이 등록되어 있지 않으면 해당 브랜드는 코디 세트에 제외되는지 검증한다")
+        @Test
+        void test2() {
+            // given
+            // 테스트 데이터 세팅
+            brandRepository.prepareTestData();
+            categoryRepository.prepareTestData();
+            productRepository.prepareTestData();
+            // D 브랜드의 TOP 상품이 없도록 세팅
+            productRepository.deleteById(25L);
+
+            List<Brand> brands = brandRepository.findAll();
+            given(brandService.findAll()).willReturn(brands);
+
+            List<Category> categories = categoryRepository.findAll();
+            given(categoryService.findAll()).willReturn(categories);
+
+            for (Brand brand : brands) {
+                for (Category category : categories) {
+                    Product product = productRepository.findTopByBrandAndCategoryOrderByPriceAsc(brand, category);
+                    if (product != null) {
+                        given(productService.findLowestPriceProductByBrandAndCategory(brand, category)).willReturn(product);
+                    } else {
+                        given(productService.findLowestPriceProductByBrandAndCategory(brand, category)).willThrow(ProductNotFoundException.class);
+                        // 해당 브랜드에서 특정 카테고리 상품이 없는 경우 더 이상 진행 않고, 다른 브랜드를 처리하도록 작성함
+                        // UnnecessaryStubbingException 예외 방지하기 위해서 break 추가
+                        break;
+                    }
+                }
+            }
+
+            // when
+            BrandCoordSet result = sut.getLowestPriceBrandProductSet();
+
+            // then
+            assertNotNull(result);
+            assertNotEquals("D", result.brandName());
+            assertNotEquals(36100L, result.totalPrice());
+            assertEquals("C", result.brandName());
+            assertEquals(37100L, result.totalPrice());
+        }
+
+        @DisplayName("브랜드가 1개도 등록되어있지 않으면 에러를 반환하는지 검증한다")
+        @Test
+        void test3() {
+            // given
+            // 테스트 데이터 세팅
+            given(brandService.findAll()).willReturn(new ArrayList<>());
+
+            // when & then
+            assertThrows(BrandNotFoundException.class, () -> sut.getLowestPriceBrandProductSet());
+        }
+    }
 }
