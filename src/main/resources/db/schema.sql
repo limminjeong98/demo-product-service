bc32f21420b0f0d9ee18e9ade77c39ef64f971d4
limminjeong98 <limminjeong98@naver.com>
2024-10-03 10:51:06 +0900

    feat: brands, categories, products 테이블 index 추가

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -5,12 +5,16 @@ CREATE TABLE brands
     CONSTRAINT pk_brands PRIMARY KEY (id)
 );
 
+CREATE INDEX idx_brands_name ON brands (id, brand_name);
+
 CREATE TABLE categories
 (
     id            BIGINT AUTO_INCREMENT NOT NULL,
     category_type VARCHAR(50)           NOT NULL,
     CONSTRAINT pk_categories PRIMARY KEY (id)
 );
+
+CREATE INDEX idx_categories_type ON categories (id, category_type);
 
 CREATE TABLE products
 (
@@ -20,3 +24,5 @@ CREATE TABLE products
     price       BIGINT                NOT NULL,
     CONSTRAINT pk_products PRIMARY KEY (id)
 );
+
+CREATE INDEX idx_products_detail ON products (id, category_id, brand_id, price);
